f55a9a7181364a97f64db0f5a37b24fa93afd8d1
Mahmoud Kassem <modykassem123@gmail.com>
2023-08-12 01:40:16 +0300

    Runtime Beats 95.36% Memory Beats 23.91%

--- 1. Two Sum/1. Two Sum.cpp
+++ 1. Two Sum/1. Two Sum.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
-	int n = nums.size();
 	unordered_map<int, int> map;
+	int n = nums.size();
 
 	for (int i = 0; i < n; i++)
 	{
-		map[nums[i]] = i;
-
 		if (map.count(target - nums[i]))
 			return { map[target - nums[i]] , i };
+
+		map[nums[i]] = i;
 	}
 
 	return {};
